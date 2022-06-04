ca790731969530a8e214cfd2072cba3d84bdadf2
vinicius claudomir <viniciusclaudomir@gmail.com>
2022-06-04 17:20:40 -0300

    feature: additional class to position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hi, i'm is test");
+		
+		Position pos = new Position(5, 3);
+		
+		System.out.println(pos);
 
 	}
 
 }
+ 
