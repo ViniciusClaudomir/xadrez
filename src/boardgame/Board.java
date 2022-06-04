2e1432d6d4e2e6f69ebe328bc6fc7ef7615121af
vinicius claudomir <viniciusclaudomir@gmail.com>
2022-06-04 18:03:01 -0300

    feature: craete print screen personalize to print pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,7 +6,7 @@ public class Board {
 	private int columns;
 	private Piece[][] pieces;
 	
-	public Board(int rows, int columns, Piece[][] pieces) {
+	public Board(int rows, int columns) {
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -28,6 +28,15 @@ public class Board {
 		this.columns = columns;
 	}
 	
+	
+	public Piece piece(int row, int column) {
+		
+		return pieces[row][column];
+	}
+	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
+	}
 	
 	
 	
